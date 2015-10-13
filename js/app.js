ae27f7b2cedaccf6e4e224c6d7bfa1593a16cf73
“Ian <ianyhkwok@hotmail.com>
2015-10-13 17:29:42 +0800

    Input field Updated

--- js/app.js
+++ js/app.js
@@ -1,16 +1,12 @@
 $(function(){
 
   var game = new Sudoku();
-
   // game.print();
   // game.swapRows(game.board[0] , game.board[1]);
-
-
   var hideRandomCell = function(){
     var cells = $('td')
-    for (var i = 0; i < 60; i++) {
+    for (var i = 0; i < 80; i++) {
       var randIndex1 = Math.floor(Math.random() * 81);
-      console.log(randIndex1)
       cells.eq(randIndex1).html("");
     }
   };
@@ -24,6 +20,7 @@ $(function(){
       $("#row-"+(i+1)).replaceWith(htmlRow);
     };
     hideRandomCell();
+
   };
 
   var showSolution = function (){
@@ -31,12 +28,41 @@ $(function(){
     for (var i = 0; i < cells.length; i++) {
       var x = cells.eq(i).attr("value");
       cells.eq(i).html(x);
-    };
+    }
+  };
+
+  var inputNum = function (event){
+    var originalValue = $(this).html();
+    $(this).html('<input type="text"></input>');
+    $(this).children().val(originalValue);
+    $("td input").css("width", "25px");
+    $(this).off();
+  };
+
+  var test = function (event){
+    $(this).parent().on('click', inputNum);
+    var inputtedNum = $(this).val();
+    $(this).parent().html(inputtedNum);
   }
 
-  // createGame();
+  // var checkResults = function(){}
+  //   //takes the input and check against the rules
+  //   //check if unique in col,row and section
+  //   var tmp = 1;
+  //   for (var i = 0; i < 9; i++){
 
-  $('#newGame').on('click', createGame);
-  $('#solution').on('click', showSolution);
+  //     for (var j = 0; j < 9; j++) {
+
+  //     }
+  //   };
+
+  // Create Game
+  $('#newGame').on('click' , createGame);
+  // Show the Solution
+  $('#solution').on('click' , showSolution);
+  // On Click to select the input box
+  $('td').on('click' , inputNum);
+  // Unfocus
+  $(document).on('blur' , 'td input', test);
 
 });
