ac8baa691447d7ea52b0b16bda0b8789d1705d5d
“Ian <ianyhkwok@hotmail.com>
2015-10-14 16:28:31 +0800

    Checking Function Completed

--- js/app.js
+++ js/app.js
@@ -6,55 +6,15 @@ $(function(){
 
   var hideRandomCell = function(){
     var cells = $('td')
-      for (var i = 0; i < 80; i++) {
+      for (var i = 0; i < 3; i++) {
         var randIndex1 = Math.floor(Math.random() * 81);
         cells.eq(randIndex1).html("");
       };
-    $('td:empty').on('click' , inputNum); // Play Game
-  };
-
-  var createGame = function(){
-    for (var i = 0; i < 9; i++){
-      var htmlRow = '<tr id="row-' + (i+1) +'">';
-        for (var j = 0; j < 9; j++) {
-          htmlRow += '<td data-num ="' + (j+1) + '" value="' + game.board[i][j] + '">' + game.board[i][j] + '</td>';
-        };
-      $("#row-"+(i+1)).replaceWith(htmlRow);
-    }
-    hideRandomCell();
-  };
-
-  var inputNum = function (event){
-    var originalValue = $(this).html();
-    $(this).html('<input type="text" autofocus></input>');
-    $(this).children().val(originalValue);
-    $(this).off();
+    $('td:empty').html('<input type="text"></input>');
     $('td input').on('keyup', checkResults);
-
   };
 
-  //Unfocus
-  var inputtedtestNum = function (event){
-    $(this).parent().on('click', inputNum);
-    var inputtedNum = $(this).val();
-    $(this).parent().html(inputtedNum);
-  };
-
-  var checkResults = function(event){
-    var answer = parseInt($('td input').val());
-    console.log(answer);
-    // if (num == valueCompare){
-    //   console.log("Yeah")
-    // } else {console.log("No")}
-  };
-
-  // Create Game
-  $('#newGame').on('click' , createGame);
-
-  // Unfocus
-  $('document').on('blur' , 'td input', inputtedtestNum);
-
-  // Show the Solution
+    // Show the Solution
   var showSolution = function (){
     var cells = $('td');
     for (var i = 0; i < cells.length; i++) {
@@ -62,5 +22,49 @@ $(function(){
       cells.eq(i).html(x);
     }
   };
+
+  var createGame = function(){
+    for (var i = 0; i < 9; i++){
+      var htmlRow = '<tr id="row-' + (i+1) +'">';
+        for (var j = 0; j < 9; j++) {
+          htmlRow += '<td data-num ="' + (j+1) +'"value="' + game.board[i][j] + '">' + game.board[i][j] + '</td>';
+        };
+      $("#row-"+(i+1)).replaceWith(htmlRow);
+    }
+    hideRandomCell();
+  };
+
+  // //Unfocus
+  // // var inputtedtestNum = function (event){
+  // //   $(this).parent().on('click', inputNum);
+  // //   var inputtedNum = $(this).val();
+  // //   $(this).parent().html(inputtedNum);
+  // // };
+
+  var checkResults = function(){
+    var input = $(this).val();
+    var value = $(this).parent().attr('value');
+    if (input == value){
+      $(this).val(input);
+      console.log("Right Answer!");
+    } else {
+      console.log("Wrong Answer! Please Try Again.");
+      $(this).val('');
+    }
+  checkgameCompleted();
+  };
+
+  var checkgameCompleted = function (){
+      if ($('input').val() == "") {
+        console.log("Game Not Completed");
+      } else {console.log("Game Completed")};
+  };
+
+  // Create Game
+  $('#easy').on('click' , createGame);
+  $('#medium').on('click' , createGame);
+
+  // Unfocus
+  // $('document').on('blur' , 'td input', inputtedtestNum);
   $('#solution').on('click' , showSolution);
 });
